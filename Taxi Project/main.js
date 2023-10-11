64a721a29736cd4a0767638042a225675dd02a25
HexoStone <hexostone@gmail.com>
2023-10-11 23:26:30 +0200

    added taxi project main js code

--- Taxi Project/main.js
+++ Taxi Project/main.js
@@ -1,15 +1,50 @@
-// Array.foreach is a different way to read array data
+// Classes are groups of attributes under custome names that can be cloned (called instances)
 
-let fruits = ["apple", "orange", "pear"];
+let musics = [
+  "Anissa",
+  "Cry Dancing",
+  "The Real Slim Shady",
+  "Never Gonna Give You Up",
+  "I Love It",
+];
+let artists = ["Wejdene", "Blessed Mane", "Eminem", "Rick Astley", "Icona Pop"];
 
-// "clasic" function expression
-console.log("\tclassic");
-fruits.forEach(function (fruit) {
-  console.log(fruit);
-});
+class Trajet {
+  constructor(radio, feux, changments) {
+    this.radio = radio;
+    this.feux = feux;
+    this.changments = changments;
+  }
 
-// "fonction fléchée" / "arrow" function expression is shorter
-console.log("\tarrow");
-fruits.forEach((fruit) => {
-  console.log(fruit);
-});
+  ranMusic() {
+    return Math.floor(Math.random() * length.musics);
+  }
+}
+
+class Personnage {
+  constructor(name, mh) {
+    this.name = name; // str
+    this.mh = mh; // int (mental health)
+  }
+
+  next(target) {
+    target.feux += 1;
+    let music = ranMusic()
+
+    if (music == 0) {
+      this.mh -= 1;
+      target.changments += 1;
+      console.log(`Oh no! ${this.name} is listening to ${musics[0]} by ${artists[0]}! He lost 1 mental health point and needs to take another taxi!`)
+    } else {
+      console.log(`${this.name} is listening to ${musics[music]} by ${artists[music]}! How cool! He can continue his ride!`)
+    }
+  }
+}
+
+// You can't call directly the class' methods, you need to create an instance first
+let juan = new Personnage("Juan", 10);
+let run = new Trajet(0, 0, 0);
+
+while (juan.mh > 0) {
+  juan.next(run);
+}
