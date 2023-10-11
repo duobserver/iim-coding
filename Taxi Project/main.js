3fa35530f327763df4adc581dbb4e6a7876fffec
HexoStone <hexostone@gmail.com>
2023-10-11 23:44:03 +0200

    fixed taxi project js

--- Taxi Project/main.js
+++ Taxi Project/main.js
@@ -17,7 +17,7 @@ class Trajet {
   }
 
   ranMusic() {
-    return Math.floor(Math.random() * length.musics);
+    return Math.floor(Math.random() * 5);
   }
 }
 
@@ -25,18 +25,32 @@ class Personnage {
   constructor(name, mh) {
     this.name = name; // str
     this.mh = mh; // int (mental health)
+    this.musics = [
+      "Anissa",
+      "Cry Dancing",
+      "The Real Slim Shady",
+      "Never Gonna Give You Up",
+      "I Love It",
+    ];
+    this.artists = ["Wejdene", "Blessed Mane", "Eminem", "Rick Astley", "Icona Pop"];
   }
 
   next(target) {
     target.feux += 1;
-    let music = ranMusic()
 
-    if (music == 0) {
+    let selection = target.ranMusic();
+    console.log(selection)
+
+    if (selection == 0) {
       this.mh -= 1;
       target.changments += 1;
-      console.log(`Oh no! ${this.name} is listening to ${musics[0]} by ${artists[0]}! He lost 1 mental health point and needs to take another taxi!`)
+      console.log(
+        `Oh no! ${this.name} is listening to ${this.musics[0]} by ${this.artists[0]}! He lost 1 mental health point and needs to take another taxi!`
+      );
     } else {
-      console.log(`${this.name} is listening to ${musics[music]} by ${artists[music]}! How cool! He can continue his ride!`)
+      console.log(
+        `${this.name} is listening to ${this.musics[selection]} by ${this.artists[selection]}! How cool! He can continue his ride!`
+      );
     }
   }
 }
@@ -45,6 +59,18 @@ class Personnage {
 let juan = new Personnage("Juan", 10);
 let run = new Trajet(0, 0, 0);
 
-while (juan.mh > 0) {
+while (juan.mh > 0 && run.feux < 30) {
   juan.next(run);
+  if (juan.mh == 0) {
+    console.log(
+      `Oh no! Juan lost his mind and died! Game over! You passed ${run.feux} traffic lights and changed taxi ${run.changments} times!`
+    );
+    break;
+  }
+  if (run.feux == 30) {
+    console.log(
+      `Hurray! Juan didn't loose his mind and arrived home! He made by changing taxi ${run.changments} times!`
+    );
+    break;
+  }
 }
