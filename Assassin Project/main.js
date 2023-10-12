2e46a717f52ed22e53dbdebde3d09a2f1bc1c8af
HexoStone <hexostone@gmail.com>
2023-10-12 17:34:24 +0200

    fixed assassin js

--- Assassin Project/main.js
+++ Assassin Project/main.js
@@ -1,5 +1,47 @@
 // Classes are groups of attributes under custome names that can be cloned (called instances)
-let names = [
+class Survivor {
+  constructor(name) {
+    this.name = name;
+    this.damage = 10;
+
+    console.log(this.name);
+  }
+}
+
+class Killer {
+  constructor() {
+    this.name = "Jason";
+    this.hp = 100;
+  }
+  next(target) {
+    let action = Math.random();
+    console.log(action);
+
+    if (action <= 0.2) {
+      this.hp -= target.damage + 5;
+      console.log(
+        `${this.name} attacks ${target.name}!\n${target.name} dealt ${
+          target.damage + 5
+        } points but died!`
+      );
+      return 1;
+    } else if (action <= 0.3) {
+      console.log(`${this.name} killed ${target.name}!`);
+      return 1;
+    } else {
+      this.hp -= target.damage;
+      console.log(
+        `${this.name} attacks ${target.name}!\nBut ${target.name} dodged him and dealt ${target.damage} points!`
+      );
+      return 0;
+    }
+  }
+}
+
+// You can't call directly the class' methods, you need to create an instance first
+let killer = new Killer();
+
+const names = [
   "James",
   "Mary",
   "Robert",
@@ -22,71 +64,32 @@ let names = [
   "Karen",
 ];
 
-class Survivor {
-  constructor(name, damage) {
-    this.name = name;
-    this.damage = damage;
-    this.status = "alive";
-  }
-}
-
-class Killer {
-  constructor() {
-    this.name = Jason;
-    this.hp = 100;
-  }
-  next(target) {
-    let action = Math.random();
-    console.log(action);
-
-    if (action <= 0.2) {
-      this.hp -= target.damage + 5;
-      target.status = "dead";
-      console.log(
-        `${this.name} attacks ${target.name}!\n${target.name} dealt ${
-          target.damage + 5
-        } points but died!`
-      );
-    } else if (action <= 0.3) {
-      target.status = "dead";
-      console.log(`${this.name} killed ${target.name}!`);
-    } else {
-      this.hp -= target.damage;
-      console.log(
-        `${this.name} attacks ${target.name}!\nBut ${target.name} dodged him and dealt ${target.damage} points!`
-      );
-    }
-  }
-}
-
-// You can't call directly the class' methods, you need to create an instance first
-let killer = new Killer();
 let survivors = [];
 
 for (let i = 0; i < 5; i++) {
-  survivors.push(new Survivor(names[Math.floor(Math.random() * names.lenght)]));
+  let name = Math.floor(Math.random() * names.length);
+  survivors.push(new Survivor(names[name]));
+  // delete names[name];
 }
 
-console.log(survivors);
+// console.log(survivors);
 
-console.log(Math.floor(Math.random() * survivors.lenght));
-
-// while (killer.hp > 0 && run.feux < 30) {
-//   killer.next(Math.floor(Math.random() * survivors.lenght));
-//   if (juan.mh == 0) {
-//     console.log(
-//       `Oh no! Juan lost his mind and exploded! Game over! He passed ${
-//         run.feux
-//       } traffic lights and rode ${run.changments + 1} taxi!`
-//     );
-//     break;
-//   }
-//   if (run.feux == 30) {
-//     console.log(
-//       `Hurray! Juan didn't loose his mind and arrived home! He made it by riding ${
-//         run.changments + 1
-//       } taxis!`
-//     );
-//     break;
-//   }
-// }
+while (killer.hp > 0 && survivors.length != 0) {
+  console.log(survivors)
+  selection = Math.floor(Math.random() * survivors.length);
+  result = killer.next(survivors[selection]);
+  if (result == 1) {
+    survivors.pop(selection);
+  }
+  if (killer.hp <= 0) {
+    console.log(`Hurray! The gang got rid of Jason!`);
+    break;
+  }
+  
+  if (survivors.length == 0) {
+    console.log(
+      `Oh no! Jason managed to kill every member of the gang! Game over!`
+    );
+    break
+  }
+}
