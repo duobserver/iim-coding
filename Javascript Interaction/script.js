8ddca6a4fa37625b33eef509dccbe32c1be946f2
duobserver <hexostone@gmail.com>
2024-01-29 13:05:30 +0100

    created beta version of project

---
+++ Javascript Interaction/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+});
