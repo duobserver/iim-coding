c961c9752d51865a0286b8b03b208d93547172ea
duobserver <hexostone@gmail.com>
2024-01-31 03:23:32 +0100

    updated files

--- Javascript Interaction/script.js
+++ Javascript Interaction/script.js
@@ -12,6 +12,37 @@ const swiper = new Swiper(".swiper", {
 });
 
 document.getElementById("sidebarButton").addEventListener("click", function () {
-  console.log("clicked")
+  console.log("clicked");
   document.getElementById("sidebar").classList.toggle("open");
+});
+
+let axes = document.querySelectorAll(".tab");
+let contents = document.querySelectorAll(".content");
+
+axes.forEach(function (item) {
+  item.addEventListener("click", function () {
+    axes.forEach(function (item) {
+      item.classList.remove("tab-active");
+    });
+    contents.forEach(function (item) {
+      item.classList.remove("active");
+    });
+    this.classList.add("tab-active");
+
+    if (this.classList.contains("tab-cdi")) {
+      document.getElementById("cdi").classList.add("active");
+    }
+    if (this.classList.contains("tab-cdeb")) {
+      document.getElementById("cdeb").classList.add("active");
+    }
+    if (this.classList.contains("tab-jv")) {
+      document.getElementById("jv").classList.add("active");
+    }
+    if (this.classList.contains("tab-3d")) {
+      document.getElementById("3d").classList.add("active");
+    }
+    if (this.classList.contains("tab-cd")) {
+      document.getElementById("cd").classList.add("active");
+    }
+  });
 });
