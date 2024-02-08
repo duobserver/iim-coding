a6582c9a911b5ee462b6be793e9b3006a4499565
duobserver <hexostone@gmail.com>
2024-02-08 14:36:59 +0100

    updated files

--- Hogwarts Collection/script.js
+++ Hogwarts Collection/script.js
@@ -49,8 +49,10 @@ function navMode() {
 // décalage de la zone principale sous la navbar
 function navHeight() {
   let main = document.getElementById("main");
+  let overlay = document.getElementById("overlay");
   let nav = document.getElementById("nav");
   main.style.paddingTop = parseFloat(nav.offsetHeight) + "px";
+  overlay.style.paddingTop = parseFloat(nav.offsetHeight) + "px";
 }
 
 // changement du mode de connexion
@@ -60,8 +62,8 @@ let forms = document.querySelectorAll("#connexionForm form");
 connexionModes.forEach(function (item) {
   item.addEventListener("click", function () {
     connexionModes.forEach(function (item) {
-      item.classList.remove("open-title")
-    })
+      item.classList.remove("open-title");
+    });
     forms.forEach(function (item) {
       item.classList.remove("active");
     });
@@ -69,6 +71,20 @@ connexionModes.forEach(function (item) {
     document.querySelector("." + item.id).classList.add("active");
   });
 });
+
+forms.forEach(function (item) {
+  item.addEventListener("submit", function (event) {
+  // sauvegarde des champs après envoi
+  event.preventDefault();
+  console.log("formulaire envoyé");
+
+  // purge de la liste des erreurs
+  document.querySelectorAll(".notif-erreur li").forEach(function (item) {
+    item.remove();
+  });
+});
+})
+
 
 // sélection dynamique des images des cartes en fonction des personnages
 let cards = document.querySelectorAll(".card");
