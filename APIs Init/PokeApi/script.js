e826f2dd09c871d188866b49dd7627920f0a6b32
duobserver <hexostone@gmail.com>
2024-03-04 01:02:13 +0100

    quick patch

--- APIs Init/PokeApi/script.js
+++ APIs Init/PokeApi/script.js
@@ -1,24 +1,18 @@
-// fetch("https://pokeapi.co/api/v2/pokemon/1002")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
 // collecting data of specific pokemon from API
 function fetchPokemon(pokemon) {
-  return fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon).then((response) => response.json());
+    return fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon).then((response) => response.json());
 }
 
 // requesting data of random pokemon and showing its name and sprite
 async function displayPokemon() {
-  let rand = Math.floor(Math.random() * 1000);
-  console.log(rand);
-  const data = await fetchPokemon(rand);
-  document.querySelector("#pokemon").innerHTML = `
-  <h1>${data.name}</h1>
-  <img src="${data.sprites.front_default}" alt="${data.name}"/>
-  `;
+    let rand = Math.floor(Math.random() * 1000);
+    console.log(rand);
+    const data = await fetchPokemon(rand);
+
+    document.querySelector("#current").innerHTML = `Current pokemon: ${data.name}`;
+    document.querySelector("#pokemon").setAttribute("src", `${data.sprites.front_default}`);
+    document.querySelector("#pokemon").setAttribute("alt", `${data.name}`);
 }
 
-// shwoing a first pokemon when the page is loaded
+// showing a first pokemon when the page is loaded
 displayPokemon();
--- APIs Init/TMDB/script.js
+++ APIs Init/TMDB/script.js
@@ -1,17 +1,17 @@
 // collecting data of specific movie from API
 function fetchMovie(movie) {
-  return fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=&language=fr-FR`).then((response) => response.json());
+    return fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=`).then((response) => response.json());
 }
 
 // requesting data of random movie and showing its name and poster
 async function displayMovie() {
-  let rand = Math.floor(Math.random() * 1000);
-  console.log(rand);
-  const data = await fetchMovie(rand);
-  document.querySelector("#movie").innerHTML = `
-  <h1>${data.title}</h1>
-  <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${data.title}"/>
-  `;
+    let rand = Math.floor(Math.random() * 100);
+    console.log(rand);
+    const data = await fetchMovie(rand);
+
+    document.querySelector("#current").innerHTML = `Current movie: ${data.title}`;
+    document.querySelector("#poster").setAttribute("src", `https://image.tmdb.org/t/p/w500${data.poster_path}`);
+    document.querySelector("#poster").setAttribute("alt", `${data.title}`);
 }
 
 // showing a first movie when the page is loaded
